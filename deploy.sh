ffa2c4f70a6ccf65b0442801e797c4da8dfc5614
zaoyifan <incredibleday@foxmail.com>
2020-06-27 20:58:51 +0800

    Update Note

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 git add .
-git update-index --assume-unchanged ./deploy.sh
-git commit -m "update note"
+git commit -m "Update Note"
+git remote rm origin
 git remote add origin git@github.com:zaoyifan/Note.git
 git push -u origin master
